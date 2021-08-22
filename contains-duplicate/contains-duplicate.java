7b982134539d5debf980b39388c0b01fa3635fdd
ajityd <85111762+ajityd@users.noreply.github.com>
2021-08-22 22:47:08 +0530

    Time: 4 ms (80.21%), Space: 43.3 MB (66.53%) - LeetHub

--- contains-duplicate/contains-duplicate.java
+++ contains-duplicate/contains-duplicate.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if(nums.length  <= 1) return false;
-        Arrays.sort(nums);
-        for(int i=0; i<nums.length-1; i++){
-            if(nums[i] == nums[i+1])
+        HashSet set = new HashSet();
+        for(int i=0; i<nums.length; i++){
+            if(!set.add(nums[i]))
                 return true;
-        }
-        if(nums[nums.length -2] == nums[nums.length-1]) return true;
-        else return false;
+            }
+        return false;
     }
 }
